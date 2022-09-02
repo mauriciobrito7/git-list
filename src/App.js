6646ea95151b7893d28883d0da8cb3f25761eb56
Mauricio Brito <maubot01@gmail.com>
2022-09-02 18:13:30 -0300

    feat: repo started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./components/layout";
+import Profile from "./components/profile";
+import Filters from "./components/filters";
+import RepoList from "./components/repo-list";
+import Search from "./components/search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Profile />
+      <Filters />
+      <RepoList />
+      <Search />
+    </Layout>
   );
 }
 
