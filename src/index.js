12cbd90208d6fa9270377bde5484bfebe3fdf579
Mauricio Brito <maubot01@gmail.com>
2022-09-03 08:44:09 -0300

    feat: global styles added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStylesStyled from './components/global-styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStylesStyled />
     <App />
   </React.StrictMode>
 );
